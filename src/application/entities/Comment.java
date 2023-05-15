dd74116b4d22a65ac75050b398343dc815fae0c6
Yan Velasco <yancarlosduartevelasco@gmail.com>
2023-05-15 11:28:15 -0300

    Class Comment created

---
+++ src/application/entities/Comment.java
@@ -0,0 +1,24 @@
+package application.entities;
+
+public class Comment {
+    private String text;
+
+    public Comment(){
+    }
+
+
+    public Comment(String text) {
+        this.text = text;
+    }
+
+
+    public String getText() {
+        return this.text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+
+}
