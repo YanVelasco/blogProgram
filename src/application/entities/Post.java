9edfffa7745b9db8334c5b1ad8386e9f9b51c9aa
Yan Velasco <yancarlosduartevelasco@gmail.com>
2023-05-15 11:40:46 -0300

    Class Post crated and the relation with class Comment

--- src/application/entities/Post.java
+++ src/application/entities/Post.java
@@ -1,1 +1,12 @@
+package application.entities;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Post {
+    private Date moment;
+    private String title;
+    private String content;
+    private Integer likes;
+    private ArrayList<Comment> comments = new ArrayList<>(); 
+}
